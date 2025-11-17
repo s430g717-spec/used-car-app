e2fa6e3570e3d95a273df1c3961ba6affbbf15cb
s430g717-spec <s430g717@gmail.com>
2025-11-17 22:52:28 +0900

    Deploy: Manual deployment

--- src/Untitled-1.ts
+++
@@ -1,9 +0,0 @@
-import { db } from './firebase';
-import { collection, getDocs } from 'firebase/firestore';
-
-const getCarData = async () => {
-  const querySnapshot = await getDocs(collection(db, 'cars'));
-  querySnapshot.forEach((doc) => {
-    console.log(doc.id, doc.data());
-  });
-};
